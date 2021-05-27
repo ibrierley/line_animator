75a8464a0eb0af8bfa6dab1e3a0a0825c136dcd9
Ian <ibrierley@gmail.com>
2021-05-27 17:20:37 +0100

    first

---
+++ example/lib/flutter_map_line_animator.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:line_animator/line_animator.dart';
+import './data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Line Animator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<LatLng> builtPoints = [];
+  double markerAngle = 0.0;
+  LatLng markerPoint;
+  List points;
+  bool isReversed = false;
+
+  @override
+  void initState() {
+    points = getPoints(1);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+        ),
+        body: LineAnimator(
+            originalPoints: points,
+            builtPoints: builtPoints,
+            duration: Duration(seconds: 5),
+            isReversed: isReversed,
+            stateChangeCallback: (status, pointList) {
+              if(status == AnimationStatus.completed) {
+                WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {isReversed = !isReversed;}));
+              }
+            },
+            duringCallback: (newPoints, point, angle, tweenVal) {
+              builtPoints = newPoints;
+              markerPoint = point;
+              markerAngle = angle;
+              WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+            },
+
+            child: FlutterMap(
+                options: MapOptions(
+                  bounds: LatLngBounds.fromPoints([...getPoints(0), ...getPoints(1)]),
+                  boundsOptions: FitBoundsOptions(padding: EdgeInsets.all(30)),
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c'],
+                  ),
+                  PolylineLayerOptions(
+                      polylines: [
+                        Polyline(
+                            points: builtPoints,
+                            strokeWidth: 2.0,
+                            color: Colors.purple
+                        ),
+                      ]
+                  ),
+
+                  MarkerLayerOptions(
+                      markers: [
+                        Marker(
+                          width: 180,
+                          height: 180,
+                          point: markerPoint,
+                          builder: (ctx) =>
+                              Container(
+                                child: Transform.rotate(
+                                    angle: markerAngle,
+                                    child: Icon(
+                                        Icons.airplanemode_active_sharp
+                                    )
+                                ),
+                              ),
+                        )
+                      ]
+                  ),
+
+                ]
+            )
+        )
+    );
+  }
+
+  @override
+  void didUpdateWidget(MyHomePage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+}
+
+
---
+++ lib/line_animator.dart
@@ -0,0 +1,239 @@
+library line_animator;
+
+import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
+import 'dart:math';
+
+class InterpolatedResult {
+  LatLng point;
+  double angle;
+  List<LatLng> builtPoints;
+  double animValue;
+  double controllerValue;
+
+  InterpolatedResult({this.point, this.angle, this.builtPoints, this.animValue, this.controllerValue});
+}
+
+class PercentageStep {
+  double percent;
+  double distance;
+  double time;
+
+  PercentageStep({this.percent, this.distance, this.time});
+}
+
+class PointInterpolator {
+
+  List<LatLng> builtPoints = [];
+  List<LatLng> points = [];
+  List<LatLng> originalPoints = [];
+  Function(LatLng, LatLng) distanceFunc;
+  List<PercentageStep> pointDistanceSteps;
+  int lastPointIndex = 1;
+  double totalDistance;
+  LatLng _previousPoint;
+  LatLng interpolatedPoint;
+  bool isReversed = false;
+
+  PointInterpolator({this.originalPoints, this.distanceFunc, this.isReversed}) {
+    reload();
+  }
+
+  void reload() {
+    lastPointIndex = 1;
+    builtPoints = [];
+    points = [];
+    buildPointsMap();
+  }
+
+  void buildPointsMap() {
+    var myDistanceFunc = distanceFunc ?? haversine;
+
+    if(isReversed) {
+      points = []..addAll(originalPoints.reversed.toList());
+    } else {
+      points = []..addAll(originalPoints);
+    }
+
+    builtPoints.add(LatLng(points[0].latitude, points[0].longitude));
+
+    pointDistanceSteps = [PercentageStep(distance: 0.0, percent: 0.0)];
+
+    totalDistance = 0.0;
+
+    /// build up a list of distances that each point must pass before being displayed
+    for (var c=0; c < points.length - 1; c++) {
+      totalDistance += myDistanceFunc(points[c], points[c+1]);
+      pointDistanceSteps.add(PercentageStep(distance: totalDistance, percent: null));
+    }
+
+    /// build a list of percentages now we know the length, for how far the point is along
+    for (var c=0; c < points.length - 2; c++) {e;
+      pointDistanceSteps[c].percent = pointDistanceSteps[c].distance / totalDistance;
+    }
+
+  }
+
+  InterpolatedResult interpolate( controllerValue, animValue, interpolateBetweenPoints ) {
+    var thisPoint;
+
+    for( var c=lastPointIndex; c < points.length - 1; c++ ) {
+      if( animValue > pointDistanceSteps[c].distance ) {
+        /// Our animation is past the next point, so add it in
+        /// but remove any interpolated point that we were using
+        if( interpolatedPoint != null ) {
+          builtPoints.removeLast(); // dont nec need the interpolated point any more
+          interpolatedPoint = null;
+        }
+
+        thisPoint = LatLng(points[c].latitude, points[c].longitude);
+        builtPoints.add(thisPoint);
+        lastPointIndex = c+1;
+      } else {
+
+        /// only need this if we want to draw inbetween points...
+        /// use our point steps and interpolate
+        if( interpolateBetweenPoints ) {
+
+          var lastPerc = pointDistanceSteps[c - 1].percent;
+          var nextPerc = pointDistanceSteps[c].percent;
+
+          if (nextPerc == null) break;
+
+          var perc = (controllerValue - lastPerc) / ///  swap this around with not 0-1 and get rid of tweener ?
+              (nextPerc - lastPerc);
+
+          var intermediateLat = (points[c].latitude -
+              points[c - 1].latitude) * perc + points[c - 1].latitude;
+          var intermediateLon = (points[c].longitude -
+              points[c - 1].longitude) * perc + points[c - 1].longitude;
+
+
+          interpolatedPoint = LatLng(
+              intermediateLat, intermediateLon); // last tail point
+
+          if (builtPoints.length >= c) {
+            builtPoints[c - 1] = interpolatedPoint;
+          } else {
+            builtPoints.add(interpolatedPoint);
+          }
+        }
+        thisPoint = interpolatedPoint;
+        break;
+      }
+    }
+
+    double angle = 0.0;
+    if(_previousPoint != null && thisPoint != null) {
+      angle = -atan2(thisPoint.latitude - _previousPoint.latitude,
+          thisPoint.longitude - _previousPoint.longitude ) - 4.7128 ;
+    }
+
+    _previousPoint = thisPoint;
+
+    return InterpolatedResult(point: thisPoint, angle: angle, animValue: animValue, controllerValue: controllerValue, builtPoints: builtPoints);
+  }
+
+  double haversine(LatLng p1, LatLng p2) {
+    var lat1 = p1.latitudeInRad,
+        lat2 = p2.latitudeInRad;
+    var lon1 = p1.longitudeInRad,
+        lon2 = p2.longitudeInRad;
+
+    var earthRadius = 6378137.0; // WGS84 major axis
+    double distance = 2 *
+        earthRadius *
+        asin(sqrt(pow(sin(lat2 - lat1) / 2, 2) +
+            cos(lat1) * cos(lat2) * pow(sin(lon2 - lon1) / 2, 2)));
+
+    return distance;
+  }
+}
+
+
+class LineAnimator extends StatefulWidget {
+  final Widget child;
+  final List originalPoints;
+  final List builtPoints;
+  final Function distanceFunc;
+  final Function stateChangeCallback;
+  final Function duringCallback;
+  final Duration duration;
+  final double begin;
+  final double end;
+  final bool isReversed;
+  final AnimationController controller;
+
+  const LineAnimator ({ Key key, this.duration, this.child, this.originalPoints, this.builtPoints, this.distanceFunc,
+    this.duringCallback,  this.stateChangeCallback,
+    this.begin=0.0, this.end, this.controller, this.isReversed }) : super(key: key);
+
+  @override
+  _LineAnimatorState createState() => _LineAnimatorState();
+}
+
+class _LineAnimatorState extends State<LineAnimator> with TickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+  bool interpolateBetweenPoints = true;
+  List<LatLng> builtPoints = [];
+  PointInterpolator interpolator;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+
+  @override
+  void initState() {
+    startAnimation();
+    super.initState();
+  }
+
+  void startAnimation() {
+    interpolator = PointInterpolator(
+originalPoints: widget.originalPoints, distanceFunc: null, isReversed: widget.isReversed);
+
+    if( controller == null)
+      controller = AnimationController(duration: widget.duration, vsync: this);
+
+    animation = Tween<double>( begin: widget.begin, end: interpolator.totalDistance ).animate(controller)
+      ..addListener(() {
+
+        InterpolatedResult interpolatedResult = interpolator.interpolate(controller.value, animation.value, true); /// not sure we need a tween at this point anymore, controller only ?
+
+        if(interpolatedResult.point != null)
+          widget.duringCallback(interpolatedResult.builtPoints, interpolatedResult.point, interpolatedResult.angle, animation.value);
+
+      })..addStatusListener((status) {
+        widget.stateChangeCallback(animation.status, builtPoints);
+      });
+
+    controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(LineAnimator oldWidget) {
+    if((oldWidget.begin != widget.begin) ||
+        (oldWidget.originalPoints != widget.originalPoints ||
+            (oldWidget.isReversed != widget.isReversed))) {
+      interpolator.originalPoints = widget.originalPoints;
+      interpolator.isReversed = widget.isReversed;
+      interpolator.reload();
+      controller.reset();
+      controller.forward(from: widget.begin);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+
+}
+
+
